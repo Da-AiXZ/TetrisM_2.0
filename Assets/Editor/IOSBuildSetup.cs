5fc18c80be2615a237c9a0e989abc565d8676c99
iOS Builder <ios-build@tetrism.local>
2026-05-31 06:22:36 +0000

    feat: add IOSDebug to mySystem in level1 via YAML

--- Assets/Editor/IOSBuildSetup.cs
+++ Assets/Editor/IOSBuildSetup.cs
@@ -1,8 +1,6 @@
 using UnityEditor;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.Rendering;
-using UnityEngine.SceneManagement;
 
 public class IOSBuildSetup
 {
@@ -15,30 +13,5 @@ public class IOSBuildSetup
         PlayerSettings.stripEngineCode = false;
         PlayerSettings.SetManagedStrippingLevel(BuildTargetGroup.iOS, ManagedStrippingLevel.Disabled);
         Debug.Log("[IOSBuildSetup] iOS configured.");
-
-        // Attach IOSDebug to level0 and level1
-        foreach (var scenePath in new[] { "Assets/Scenes/level0.unity", "Assets/Scenes/level1.unity" })
-        {
-            var scene = EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Single);
-            var mySystem = GameObject.Find("mySystem");
-            if (mySystem == null)
-            {
-                var mainCam = GameObject.Find("Main Camera");
-                if (mainCam != null)
-                {
-                    var debug = mainCam.GetComponent<IOSDebug>();
-                    if (debug == null) mainCam.AddComponent<IOSDebug>();
-                    Debug.Log($"[IOSBuildSetup] IOSDebug added to Main Camera in {scenePath}");
-                }
-            }
-            else
-            {
-                var debug = mySystem.GetComponent<IOSDebug>();
-                if (debug == null) mySystem.AddComponent<IOSDebug>();
-                Debug.Log($"[IOSBuildSetup] IOSDebug added to mySystem in {scenePath}");
-            }
-            EditorSceneManager.MarkSceneDirty(scene);
-            EditorSceneManager.SaveScene(scene);
-        }
     }
 }
