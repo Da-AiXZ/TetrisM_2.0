fd89ccae730b68441f947cd521a7a2a0bc92840d
iOS Builder <ios-build@tetrism.local>
2026-06-01 15:13:53 +0000

    diag: add CanvasScaler status + touch raycast count/position

--- Assets/Scripts/Assembly-CSharp/TouchInputModule.cs
+++ Assets/Scripts/Assembly-CSharp/TouchInputModule.cs
@@ -8,7 +8,9 @@ public class TouchInputModule : BaseInputModule
 	private List<RaycastResult> raycastResults = new List<RaycastResult>();
 	private int processCount = 0;
 	private int touchCount = 0;
+	private int rayCount = 0;
 	private string lastHit = "";
+	private string lastTouchPos = "";
 
 	public override void Process()
 	{
@@ -17,6 +19,7 @@ public class TouchInputModule : BaseInputModule
 		touchCount = Input.touchCount;
 
 		var touch = Input.GetTouch(0);
+		lastTouchPos = $"{touch.position.x:F0},{touch.position.y:F0}";
 
 		if (pointerData == null)
 			pointerData = new PointerEventData(eventSystem);
@@ -26,9 +29,16 @@ public class TouchInputModule : BaseInputModule
 
 		raycastResults.Clear();
 		eventSystem.RaycastAll(pointerData, raycastResults);
+		rayCount = raycastResults.Count;
 
 		if (raycastResults.Count > 0)
-			lastHit = raycastResults[0].gameObject != null ? raycastResults[0].gameObject.name : "null";
+		{
+			lastHit = $"{raycastResults[0].gameObject.name}:{raycastResults[0].gameObject.layer}";
+		}
+		else
+		{
+			lastHit = "NONE";
+		}
 
 		var firstTarget = raycastResults.Count > 0 ? raycastResults[0] : default(RaycastResult);
 
@@ -67,6 +77,6 @@ public class TouchInputModule : BaseInputModule
 
 	public string GetDiag()
 	{
-		return $"timProc={processCount} timTouch={touchCount} timHit={lastHit}";
+		return $"timProc={processCount} timTouch={touchCount} timRay={rayCount} timHit={lastHit} timPos={lastTouchPos}";
 	}
 }
